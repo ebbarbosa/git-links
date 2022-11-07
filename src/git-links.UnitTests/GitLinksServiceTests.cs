65ba694cf49f8e6dbca27bf700d21f34bc952bd8
ebarbosa-bp <eduardo.barbosa@blueprism.com>
2022-11-07 15:45:32 +0000

    refactor folder structure

---
+++ src/git-links.UnitTests/GitLinksServiceTests.cs
@@ -0,0 +1,17 @@
+﻿namespace git_links.UnitTests
+{
+    public class GitLinksServiceTests
+    {
+        private GitLinksService _service;
+
+        [Fact]
+        public void GetFeatureBranch_Replaces_ForwardSlash_With_Dash()
+        {
+            _service = new GitLinksService("https://blueprism.atlassian.net/browse/HUB-6295", "Create release/5.0.0 branch for hub-authenticationserver ", TaskTypes.Feature);
+            var expected = "feature/HUB-6295-create-release-5.0.0-branch-for-hub-authenticationserver";
+            var actual = _service.GetFeatureBranch();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
